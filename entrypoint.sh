00ac5ec29c890b393b0a8714d072e0322affd4df
X-osadmin <9164356+xosadmin@users.noreply.github.com>
2024-11-18 14:38:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ if [ -z "$MYSQL_HOST" ] || [ -z "$MYSQL_DBNAME" ] || [ -z "$MYSQL_USER" ] || [ -
 fi
 
 echo "Starting rsyslog..."
+sed -i 's/^module(load="imklog")/#module(load="imklog")/' /etc/rsyslog.conf
 if ! grep -q 'module(load="ommysql")' /etc/rsyslog.conf; then
   echo "module(load=\"ommysql\")" >> /etc/rsyslog.conf
-  echo "*.* :ommysql:${MYSQL_HOST},${MYSQL_DBNAME},${MYSQL_USER},${MYSQL_PASSWORD};RSYSLOG_TEMPLATE" >> /etc/rsyslog.conf
   echo "template(name=\"RSYSLOG_TEMPLATE\" type=\"string\" string=\"%TIMESTAMP% %HOSTNAME% %fromhost-ip% %programname% %syslogseverity-text% %msg%\n\")" >> /etc/rsyslog.conf
   echo "*.* :ommysql:${MYSQL_HOST},${MYSQL_DBNAME},${MYSQL_USER},${MYSQL_PASSWORD};RSYSLOG_TEMPLATE" >> /etc/rsyslog.conf
 fi
